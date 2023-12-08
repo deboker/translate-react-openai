6d6a174dff4446211183b61d1353528ebd8590d6
deboker <117391017+deboker@users.noreply.github.com>
2023-12-08 21:43:07 +0100

    changing open ai to huggingface

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
-import OpenAI from "openai";
+import { HfInference } from "@huggingface/inference";
+
 import crazyParot from "./assets/crazy-parot.png";
 
 const App = () => {
@@ -9,34 +10,36 @@ const App = () => {
   const [loading, setLoading] = useState(false);
 
   const handleTranslate = async () => {
-    const openaiApiKey = import.meta.env.VITE_OPENAI_API_KEY;
-
-    const messages = [
-      {
-        role: "system",
-        content: `You are a translator from detected language from inputText language to language:${selectedLanguage} and when is ${!selectedLanguage} language write Please choose language!`,
-      },
-      {
-        role: "user",
-        content: `${inputText}`,
-      },
-    ];
-
     try {
-      const openai = new OpenAI({
-        apiKey: openaiApiKey,
-        dangerouslyAllowBrowser: true,
+      const hf = new HfInference(import.meta.env.VITE_HF_TOKEN);
+
+      const languageCodeMap = {
+        French: "fr_XX",
+        Spanish: "es_XX",
+        Japanese: "ja_XX",
+        // Add more languages as needed
+      };
+
+      const response = await hf.translation({
+        model: "facebook/mbart-large-50-many-to-many-mmt",
+        inputs: inputText,
+        parameters: {
+          src_lang: "en_XX",
+          tgt_lang: languageCodeMap[selectedLanguage],
+        },
       });
-      const response = await openai.chat.completions.create({
-        model: "gpt-4",
-        messages: messages,
-        temperature: 1,
-        presence_penalty: 0,
-        frequency_penalty: 0,
-      });
-      setTranslatedText(response.choices[0].message.content);
+
+      if (response && response.translation_text) {
+        setTranslatedText(response.translation_text);
+      } else {
+        setTranslatedText("Translation error. Please try again.");
+      }
+      console.log(response);
     } catch (err) {
+      console.error("Translation error:", err);
       setTranslatedText("Translation error. Please try again.");
+    } finally {
+      setLoading(false);
     }
   };
 
@@ -77,7 +80,7 @@ const App = () => {
                   <input
                     type="radio"
                     name="language"
-                    value="French"
+                    value="fr_XX"
                     checked={selectedLanguage === "French"}
                     onChange={() => setSelectedLanguage("French")}
                   />
@@ -87,7 +90,7 @@ const App = () => {
                   <input
                     type="radio"
                     name="language"
-                    value="Spanish"
+                    value="es_XX"
                     checked={selectedLanguage === "Spanish"}
                     onChange={() => setSelectedLanguage("Spanish")}
                   />
@@ -97,7 +100,7 @@ const App = () => {
                   <input
                     type="radio"
                     name="language"
-                    value="Japanese"
+                    value="ja_XX"
                     checked={selectedLanguage === "Japanese"}
                     onChange={() => setSelectedLanguage("Japanese")}
                   />
@@ -114,7 +117,11 @@ const App = () => {
             className="submit-button"
             onClick={translatedText ? handleStartOver : handleTranslate}
           >
-            {translatedText ? "Start Over" : "Translate"}
+            {loading
+              ? "Translating..."
+              : translatedText
+              ? "Start Over"
+              : "Translate"}
           </button>
         </div>
       </div>
